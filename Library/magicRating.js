ef69a3f55a3eccc9e14e241784c11a9916563af2
Dino <dino.raineri@gmail.com>
2016-12-28 18:52:55 +0100

    icons + mouvement update css

---
+++ Library/magicRating.js
@@ -0,0 +1,65 @@
+// Init function
+$.fn.ratingInit = function(){
+    var currentRating = $(this).data("currentRating");
+
+    // Clear
+    $(this).html("");
+
+    // Init icons
+    for(i=1; i<=5; i++) // TODO: number of rate star
+    {
+        if(i <= currentRating)
+        {
+            $(this).append('<i class="fa fa-star magic-rating-icon" aria-hidden="true" data-default=true data-rating='+ i +'></i>');        
+        }
+        else
+        {
+            $(this).append('<i class="fa fa-star-o magic-rating-icon" aria-hidden="true" data-default=false data-rating='+ i +'></i>');        
+        }
+    }
+
+    // Init reset handler
+    $(this).on("mouseleave", function () {
+        $(this).children().each(function(){
+            var icon = $(this);
+            if(icon.data("default") && !icon.hasClass("fa-star"))
+            {
+                icon.removeClass("fa-star-o");
+                icon.addClass("fa-star");
+            }
+            else if(!icon.data("default") && !icon.hasClass("fa-star-o"))
+            {
+                icon.removeClass("fa-star");
+                icon.addClass("fa-star-o");   
+            }
+        });
+    });
+
+    return this;
+};
+
+// Update for hover icons
+$(document).on("mouseenter", ".magic-rating-icon", function(){
+    var icon = $(this);
+    var rating = icon.data("rating");
+    icon.parent().children().each(function(){
+        if ($(this).data("rating") <= rating) 
+        {
+            if (!$(this).hasClass("fa-star")) 
+            {
+                $(this).removeClass("fa-star-o");
+                $(this).addClass("fa-star");
+            };
+        }
+        else
+        {
+            if(!$(this).hasClass("fa-star-o"))
+            {
+                $(this).removeClass("fa-star");
+                $(this).addClass("fa-star-o");
+            }
+        }
+    });
+});
+
+// Click system
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MagicRating
 A javascript library to create a rating system front-end
+
+# Dependencies
+* Jquery (tested with [Jquery 3.1.1](https://code.jquery.com/jquery-3.1.1.min.js))
+* FontAwesome (tested with  [FontAwesome4.7.0](https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css))
