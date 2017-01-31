e46c350d17c5c48ee9e658e50e3007a39cf79cd2
Dino <dino.raineri@gmail.com>
2017-01-31 11:11:36 +0100

    add readOnly and multi classes

--- Library/magicRating.js
+++ Library/magicRating.js
@@ -9,16 +9,26 @@ $.fn.magicRatingInit = function(config){
         // Icon +
         if (magicRatingWidget.data("iconGood") == null) {
             magicRatingWidget.data("iconGood", config.iconGood != null ? config.iconGood : "fa-star");       
-        };
+        }
 
         // Icon -
         if (magicRatingWidget.data("iconBad") == null) {
-            magicRatingWidget.data("iconBad", config.iconBad != null ? config.iconBad : "fa-star-o");      
-        };
+            magicRatingWidget.data("iconBad", config.iconBad != null ? config.iconBad : "fa-star-o");
+        }
 
         // Max mark
         if(magicRatingWidget.data("maxMark") == null){
             magicRatingWidget.data("maxMark", config.maxMark != null ? config.maxMark : 5);
+        }
+
+        // Current rating
+        if(magicRatingWidget.data("currentRating") == null){
+            magicRatingWidget.data("currentRating", config.currentRating != null ? config.currentRating : 0);
+        }
+
+        // Read only
+        if(magicRatingWidget.data("readOnly") == null){
+            magicRatingWidget.data("readOnly", config.readOnly != null ? config.readOnly : false);
         }
         
         /*
@@ -30,7 +40,7 @@ $.fn.magicRatingInit = function(config){
         magicRatingWidget.html("");
 
         // Init icons
-        for(i=1; i<=magicRatingWidget.data("maxMark"); i++)
+        for(var i=1; i<=magicRatingWidget.data("maxMark"); i++)
         {
             if(i <= magicRatingWidget.data("currentRating"))
             {
@@ -42,97 +52,86 @@ $.fn.magicRatingInit = function(config){
             }
         }
 
-        // Init reset handler
-        magicRatingWidget.on("mouseleave", function () {
-            var widget = $(this);
-            /*
-            console.log("mouseleave");
-            console.log(widget.data("iconGood"));
-            console.log(widget.data("iconBad"));
-            */
-            widget.children().each(function(){
-                var icon = $(this);
-                if(icon.data("default") && !icon.hasClass("fa-star"))
-                {
-                    icon.removeClass(widget.data("iconBad"));
-                    icon.addClass(widget.data("iconGood"));
-                }
-                else if(!icon.data("default") && !icon.hasClass("fa-star-o"))
-                {
-                    icon.removeClass(widget.data("iconGood"));
-                    icon.addClass(widget.data("iconBad"));   
-                }
-            });
-        });
-
-        // Init click handler
-        magicRatingWidget.on("click", ".magic-rating-icon", function()
+        if(!magicRatingWidget.data("readOnly"))
         {
-            // Get rating
-            var icon = $(this);
-            var widget = icon.parent();
-            var rating = icon.data("rating");
-            /*
-            console.log("click");
-            console.log(widget.data("iconGood"));
-            console.log(widget.data("iconBad"));
-            */
-            // Update rating
-            widget.children().each(function(){
-                if ($(this).data("rating") <= rating) 
-                {
-                    if (!$(this).hasClass(widget.data("iconGood"))) 
+            // Init reset handler
+            magicRatingWidget.on("mouseleave", function () {
+                var widget = $(this);
+
+                // console.log("mouseleave");
+                // console.log(widget.data("iconGood"));
+                // console.log(widget.data("iconBad"));
+
+                widget.children().each(function(){
+                    var icon = $(this);
+                    if(icon.data("default"))
+                    {
+                        icon.removeClass(widget.data("iconBad"));
+                        icon.addClass(widget.data("iconGood"));
+                    }
+                    else if(!icon.data("default"))
+                    {
+                        icon.removeClass(widget.data("iconGood"));
+                        icon.addClass(widget.data("iconBad"));   
+                    }
+                });
+            });
+
+            // Init click handler
+            magicRatingWidget.on("click", ".magic-rating-icon", function()
+            {
+                // Get rating
+                var icon = $(this);
+                var widget = icon.parent();
+                var rating = icon.data("rating");
+
+                // console.log("click");
+                // console.log(widget.data("iconGood"));
+                // console.log(widget.data("iconBad"));
+
+                // Update rating
+                widget.children().each(function(){
+                    if ($(this).data("rating") <= rating) 
                     {
                         $(this).removeClass(widget.data("iconBad"));
                         $(this).addClass(widget.data("iconGood"));
-                    };
-                    $(this).data("default", true);
-                }
-                else
-                {
-                    if(!$(this).hasClass(widget.data("iconBad")))
+                        $(this).data("default", true);
+                    }
+                    else
+                    {
+                        $(this).removeClass(widget.data("iconGood"));
+                        $(this).addClass(widget.data("iconBad"));
+                        $(this).data("default", false);
+                    }
+                });
+
+                var callbackSuccess = config.success.bind(null, widget, rating);
+                callbackSuccess();
+            });
+
+            // Init hover icons
+            magicRatingWidget.on("mouseenter", ".magic-rating-icon", function(){
+                var icon = $(this);
+                var rating = icon.data("rating");
+                var widget = icon.parent();
+
+                // console.log("mouseenter");
+                // console.log(widget.data("iconGood"));
+                // console.log(widget.data("iconBad"));
+
+                widget.children().each(function(){
+                    if ($(this).data("rating") <= rating) 
+                    {
+                        $(this).removeClass(widget.data("iconBad"));
+                        $(this).addClass(widget.data("iconGood"));
+                    }
+                    else
                     {
                         $(this).removeClass(widget.data("iconGood"));
                         $(this).addClass(widget.data("iconBad"));
                     }
-                    $(this).data("default", false);
-                }
+                });
             });
-
-            var callbackSuccess = config.success.bind(null, widget, rating);
-            callbackSuccess();
-        });
-
-        // Init hover icons
-        magicRatingWidget.on("mouseenter", ".magic-rating-icon", function(){
-            var icon = $(this);
-            var rating = icon.data("rating");
-            var widget = icon.parent();
-            /*
-            console.log("mouseenter");
-            console.log(widget.data("iconGood"));
-            console.log(widget.data("iconBad"));
-            */
-            widget.children().each(function(){
-                if ($(this).data("rating") <= rating) 
-                {
-                    if (!$(this).hasClass(widget.data("iconGood"))) 
-                    {
-                        $(this).removeClass(widget.data("iconBad"));
-                        $(this).addClass(widget.data("iconGood"));
-                    };
-                }
-                else
-                {
-                    if(!$(this).hasClass(widget.data("iconBad")))
-                    {
-                        $(this).removeClass(widget.data("iconGood"));
-                        $(this).addClass(widget.data("iconBad"));
-                    }
-                }
-            });
-        });
+        }        
     }    
 };
-
-// Update for hover icons
